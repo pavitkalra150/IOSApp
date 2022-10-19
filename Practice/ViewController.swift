d80c6b0f0561c1f18b5a06d9c8602d73b351ea6e
pavitkalra150 <c0868150@mylambton.ca>
2022-10-18 23:03:42 -0400

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by PAVIT KALRA on 2022-10-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
